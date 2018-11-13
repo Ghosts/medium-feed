3a2e66a3d65e6d0bf40a8a7fa6650c81af9525fc
Caden Sumner <cadenjsumner@gmail.com>
2018-11-13 17:01:01 -0500

    Setting up package & gulp

---
+++ dist/medium-feed.js
@@ -0,0 +1,12 @@
+var MediumFeed = function() {
+    this.articles = [];
+}
+
+MediumFeed.prototype = {
+    getFeed: function(username) {
+        
+    },
+    clearArticles: function() {
+        this.articles = [];
+    },
+}
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+const minify = require('gulp-minify');
+
+gulp.task('compress', () => {
+    return gulp.src(['lib/*.js', 'lib/*.mjs'])
+      .pipe(minify({
+          ext:{
+              src:'.js',
+              min:'.min.js'
+          },
+      }))
+      .pipe(gulp.dest('dist'))
+  });
+
+gulp.task('default', gulp.series('compress'), () => {});
